66d0022b0ee43f8ea2227b237a135ce0fc11f006
Nissan Pow <nissan.pow@gmail.com>
2026-03-10 22:01:41 -0700

    fix: resolve IATA code collisions and sort active airlines first

--- convert.js
+++ convert.js
@@ -5,15 +5,22 @@ var _ = require('lodash');
 
 var columns = ['id', 'name', 'alias', 'iata', 'icao', 'callsign', 'country', 'active'];
 
-var readStream = fs.createReadStream('airlines.dat');
-var writeStream = fs.createWriteStream('airlines.json');
+var content = fs.readFileSync('airlines.dat', 'utf8');
 
-var transformer = csv.transform(function(data) {
-  return _.zipObject(columns, data);
+csv.parse(content, function(err, records) {
+  if (err) throw err;
+
+  var airlines = records.map(function(data) {
+    return _.zipObject(columns, data);
+  });
+
+  // Sort active airlines first so they take precedence over inactive ones
+  // when consumers look up by IATA/ICAO code
+  airlines = _.sortBy(airlines, function(a) { return a.active === 'Y' ? 0 : 1; });
+
+  var writeStream = fs.createWriteStream('airlines.json');
+  var jsonStream = JSONStream.stringify();
+  jsonStream.pipe(writeStream);
+  airlines.forEach(function(a) { jsonStream.write(a); });
+  jsonStream.end();
 });
-
-readStream
-  .pipe(csv.parse())
-  .pipe(transformer)
-  .pipe(JSONStream.stringify())
-  .pipe(writeStream);
--- normalize.js
+++ normalize.js
@@ -41,6 +41,44 @@ var ID_CORRECTIONS = {
   '3386':  { active: 'N' },  // Malmö Aviation (dup of above, same ICAO SCW)
   '4264':  { active: 'N' },  // Royal Nepal Airlines (defunct, replaced by Nepal Airlines)
   '4560':  { active: 'N' },  // Swissair (defunct, dup of Swiss Intl, same ICAO SWR)
+  // IATA code collision fixes - defunct airlines sharing code with active ones:
+  '595':   { active: 'N' },  // Atlant-Soyuz Airlines (3G) - ceased 2011
+  '19862': { active: 'N' },  // AsiaCargo Express (3G) - rebranded/ceased 2020
+  '2051':  { active: 'N' },  // DonbassAero (5D) - ceased 2013
+  '3432':  { active: 'N' },  // Maxair (8M) - ceased 2005
+  '3570':  { active: 'N' },  // Myanmar Airways International (8M) - duplicate entry (keep id 19846)
+  '1436':  { active: 'N' },  // Bellview Airlines (B3) - ceased 2009
+  '543':   { active: 'N' },  // Air Bangladesh (B9) - ceased 2005
+  '1411':  { active: 'N' },  // British International Helicopters (BS) - ceased 2012
+  '3363':  { active: 'N' },  // Macair Airlines (CC) - ceased 2009
+  '1881':  { active: 'N' },  // Continental Airlines (CO) - merged into United 2012
+  '1883':  { active: 'N' },  // Continental Express (CO) - defunct
+  '1615':  { active: 'N' },  // Canadian Airlines (CP) - bankrupt 2001
+  '1860':  { active: 'N' },  // Compass Airlines (CP) - ceased 2020
+  '1790':  { active: 'N' },  // City Connexion Airlines (G3) - ceased ~2000
+  '116':   { active: 'N' },  // Air Italy (I9) - liquidated 2020
+  '2855':  { active: 'N' },  // Indigo/IBU (I9) - ceased 2004
+  '4188':  { active: 'N' },  // Republic Express Airlines (RH) - ceased 2010
+  '4936':  { active: 'N' },  // Tiger Airways (TR) - merged into Scoot 2017
+  '2439':  { active: 'N' },  // Formosa Airlines (VY) - merged 1999
+  '5424':  { active: 'N' },  // Western Airlines (WA) - merged into Delta 1987
+  '575':   { active: 'N' },  // Air Exel (XT) - ceased 2005
+  '792':   { active: 'N' },  // Access Air (ZA) - ceased 2001
+  '2883':  { active: 'N' },  // Interavia Airlines (ZA) - ceased 2008
+  '220':   { active: 'N' },  // Air Bourbon (ZB) - ceased 2004
+  '3532':  { active: 'N' },  // Monarch Airlines (ZB) - collapsed 2017
+  '3743':  { active: 'N' },  // Novair (1I) - bankrupt 2023
+  '3788':  { active: 'N' },  // Onur Air (8Q) - bankrupt 2021
+  '5559':  { active: 'N' },  // Maldivian Air Taxi (8Q) - merged into Trans Maldivian 2013
+  '1879':  { active: 'N' },  // Contact Air (C3) - ceased 2012
+  '12997': { active: 'N' },  // QatXpress (C3) - defunct
+  '19861': { active: 'N' },  // Regent Airways (RX) - suspended 2020
+  '19676': { active: 'N' },  // Rainbow Air Polynesia (RX) - defunct
+  '4981':  { active: 'N' },  // Trans Mediterranean Airlines (TL) - ceased 2014
+  '11850': { active: 'N' },  // Skyjet Airlines (UQ) - ceased 2009
+  // Wrong IATA codes in upstream data:
+  '4374':  { iata: 'GQ' },  // Sky Express Greece - correct code is GQ not G3
+  '19849': { iata: 'GX' },  // Guangxi Beibu Gulf Airlines - correct code is GX not UQ
 };
 
 var columns = ['id', 'name', 'alias', 'iata', 'icao', 'callsign', 'country', 'active'];
